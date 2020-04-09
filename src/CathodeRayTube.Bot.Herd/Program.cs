b6968b9c66969b6987ccf03ec6762deb09aa66ef
crt-tv <crt-tv@users.noreply.github.com>
2020-04-09 12:13:53 -0500

    Add project files.

---
+++ src/CathodeRayTube.Bot.Herd/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CathodeRayTube.Bot.Herd
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Debug);
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        }
+    }
+}
---
+++ src/CathodeRayTube.Bot.Twitch/TwitchBotService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CathodeRayTube.Bot.Twitch
+{
+    public class TwitchBotService : BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<TwitchBotService> _logger;
+
+        public TwitchBotService(IServiceProvider serviceProvider, ILogger<TwitchBotService> logger)
+        {
+            _serviceProvider = serviceProvider;
+            _logger = logger;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogDebug("Executing asynchronusly");
+
+            return Task.CompletedTask;
+        }
+    }
+}
